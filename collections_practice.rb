839657bc9908a0beaa3f120568c3c2e20a411c80
Chukwuemeka Kanu <chukwuemekakanu@gmail.com>
2020-02-03 15:49:39 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,24 @@
+def sort_array_asc(numbers)
+  numbers.sort
+end
 
+def sort_array_desc(numbers)
+  numbers.sort.reverse
+end  
+
+def sort_array_char_count (array)
+ array.sort { |a, b| a.size <=> b.size }
+end
+
+def swap_elements(array)
+  array[0], array[1],array[2] = array[0], array[2],array[1]
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+array.delete_at(2) 
+array.insert(2,"$")
+end
